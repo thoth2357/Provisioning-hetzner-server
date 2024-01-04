8c3c078f55d5b6ababf1775beb1c8d1d8bc02286
thoth2357 <thoth2357@gmail.com>
2024-01-04 14:19:17 +0100

    :pencil2: Fix typo

--- ssh.tf
+++ ssh.tf
@@ -3,7 +3,7 @@ resource "tls_private_key" "generic-ssh-key" {
 	rsa_bits  = 4096
 }
 
-resource "hcloud_ssh_key" "primary-shh-key" {
+resource "hcloud_ssh_key" "primary-ssh-key" {
 	name = "primary-ssh-key"
 	public_key = tls_private_key.generic-ssh-key.public_key_openssh
 }
