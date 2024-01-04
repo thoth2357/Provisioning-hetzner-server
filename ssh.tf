06bcd40431a36b14cbcaf1395a4b25a67d7ccb5f
thoth2357 <thoth2357@gmail.com>
2024-01-04 11:06:05 +0100

    :wrench: Added Configuration for ssh key

---
+++ ssh.tf
@@ -0,0 +1,9 @@
+resource "tls_private_key" "generic-ssh-key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "hcloud_ssh_key" "primary-shh-key" {
+    name = "primary-ssh-key"
+    public_key = tls_private_key.generic-ssh-key.public_key_openssh
+}
