1ab88d2b15f005b44824701ded564621c6967ead
thoth2357 <thoth2357@gmail.com>
2024-01-04 13:11:27 +0100

    :wrench: Added server terraform block

---
+++ server.tf
@@ -0,0 +1,11 @@
+resource "hcloud_server" "Web" {
+    name        = "controller"
+    server_type = var.server_type
+    image       = var.os_image
+    location    = var.location
+    ssh_keys    = [hcloud_ssh_key.primary-ssh-key.name]
+    labels = {
+        purpose = "OSI-LMS"
+    }
+    user_data = file("cloud-config.yml")
+}
