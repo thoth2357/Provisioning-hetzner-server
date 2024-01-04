97ccfb1bfd88ab45731d7130cd467d2451a9f4c2
thoth2357 <thoth2357@gmail.com>
2024-01-04 13:21:36 +0100

    :wrench: Added server instance config

--- instance.tf
+++ instance.tf
@@ -3,6 +3,7 @@ resource "hcloud_server" "Web" {
     server_type = var.server_type
     image       = var.os_image
     location    = var.location
+    backups = "false"
     ssh_keys    = [hcloud_ssh_key.primary-ssh-key.name]
     labels = {
         purpose = "OSI-LMS"
