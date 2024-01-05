06797faa1b6845da6ef19ee6b9c912bc3e19ad02
thoth2357 <thoth2357@gmail.com>
2024-01-05 13:23:20 +0100

    :ambulance: :lock: switched from using tls private key provider to providing pub through file

--- ssh.tf
+++ ssh.tf
@@ -1,9 +1,4 @@
-resource "tls_private_key" "generic-ssh-key" {
-	algorithm = "RSA"
-	rsa_bits  = 4096
-}
-
 resource "hcloud_ssh_key" "primary-ssh-key" {
-	name = "primary-ssh-key"
-	public_key = tls_private_key.generic-ssh-key.public_key_openssh
+	name = "seyi-ssh-key"
+	public_key = file("~/.ssh/OSI-LMS.pub")
 }
