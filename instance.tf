42764a868378568fadda657d67e9aec7abd3b601
thoth2357 <thoth2357@gmail.com>
2024-01-05 13:10:53 +0100

    :pencil2: Rename ssh_keys, changed server name and  added type and purpose labels

--- instance.tf
+++ instance.tf
@@ -1,12 +1,13 @@
-resource "hcloud_server" "Web" {
-    name        = "controller"
+resource "hcloud_server" "webserver" {
+    name        = "web-server"
     server_type = var.server_type
     image       = var.os_image
     location    = var.location
     backups = "false"
-    ssh_keys    = [hcloud_ssh_key.primary-ssh-key.name]
+    ssh_keys    = [hcloud_ssh_key.primary-ssh-key.id]
     labels = {
-        purpose = "OSI-LMS"
+        type = "web"
+        purpose = "OSI-LMS-SERVER"
     }
     user_data = file("cloud-config.yml")
 }
