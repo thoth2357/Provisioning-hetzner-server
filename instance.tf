af3c1373e0d44c57f4d2137368ebb73457103725
thoth2357 <thoth2357@gmail.com>
2024-01-13 20:44:05 +0100

    :hammer: Added new ssh key

--- instance.tf
+++ instance.tf
@@ -5,7 +5,7 @@ resource "hcloud_server" "webserver" {
     location    = var.location
     backups = "false"
     firewall_ids = [hcloud_firewall.default.id, hcloud_firewall.default-egress.id]
-    ssh_keys    = [hcloud_ssh_key.primary-ssh-key.id, hcloud_ssh_key.secondary-ssh-key.id]
+    ssh_keys    = [hcloud_ssh_key.primary-ssh-key.id, hcloud_ssh_key.secondary-ssh-key.id, hcloud_ssh_key.secondary2-ssh-key]
     labels = {
         type = "web"
         purpose = "OSI-Dev-Server"
